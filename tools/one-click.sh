762b74b6ea2ee6d0ffdda205934dac286b5f9600
YuLong <15810397896@163.com>
2021-04-15 10:56:54 +0800

    feature: 一键部署 LDAP 客户端

--- tools/one-click.sh
+++ tools/one-click.sh
@@ -7,95 +7,55 @@
 # Describe: 一键式配置 Linux 服务器适配 LDAP SSH-KEY 与 SUDO
 #########################################################################
 
-LDAP_FQDN="ldap-server.host.com"
-LDAP_PORT=389
-LDAP_URI="ldap://${LDAP_FQDN}:${LDAP_PORT}"
+LDAP_FQDN="ldap-server.magic.com"
+LDAP_URI="ldap://${LDAP_FQDN}"
 LDAP_BASE_DN="dc=magic,dc=com"
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
 # REMOTE_ADDRESS=(192.168.1.72 192.168.1.97 192.168.1.98 192.168.1.151 192.168.1.152 192.168.1.153 192.168.1.67 192.168.1.68)
-REMOTE_ADDRESS=(192.168.1.71)
+REMOTE_ADDRESS=(192.168.1.153)
 
 
-sudo_conf(){
-
+install_ldap_client(){
     for ip in "${REMOTE_ADDRESS[@]}"
     do
-        echo -e "Copy Run Remote Server ${ip}\n"
-        # 备份文件
-        ssh root@"${ip}" 'cp /etc/sudo-ldap.conf /etc/sudo-ldap.conf.${DATE}'
-        ssh root@"${ip}" 'cp /etc/nsswitch.conf /etc/nsswitch.conf.${DATE}'
-        # 写人文件
-        scp sudo-ldap.conf  root@"${ip}":/etc/sudo-ldap.conf
-        ssh root@"${ip}" 'echo "sudoers:    files   ldap" >> /etc/nsswitch.conf'
-        echo -e "\n"
+        ssh root@${ip} "yum install -y openldap-clients nss-pam-ldapd > /dev/null"
     done
 }
 
-ssh_setup(){
+setup_client_authconfig(){
+    # 配置使用 TLS
+    ssh root@${ip} "systemctl restart nslcd && systemctl status nslcd"
+    ssh root@${ip} "[[ ! -d /etc/openldap/cacerts ]] && mkdir /etc/openldap/cacerts"
+    scp magicCA.pem root@${ip}:/etc/openldap/cacerts/magicCA.pem
+    ssh root@${ip} "authconfig --enableldap --enableldapauth --enableldaptls --ldapserver=${LDAP_URI} --ldapbasedn=${LDAP_BASE_DN} --enablemkhomedir --update"
+}
+
+config_rsync(){
     for ip in "${REMOTE_ADDRESS[@]}"
     do
-        echo -e "Setup SSH LDAP Run Remote Server ${ip}"
-        ssh root@"${ip}" ' yum -y install openssh-ldap'
-        scp magicCA.pem root@"${ip}":/etc/openldap/cacerts
-        scp ldap.conf root@"${ip}":/etc/ssh/
-        ssh root@"${ip}" 'echo -e "AuthorizedKeysCommand /usr/libexec/openssh/ssh-ldap-wrapper\nAuthorizedKeysCommandUser nobody\nPubkeyAuthentication yes" >> /etc/ssh/sshd_config'
-        echo -e "\n"
+        scp ldap.conf root@${ip}:/etc/openldap/ldap.conf
+        scp nslcd.conf root@${ip}:/etc/nslcd.conf
+        scp nsswitch.conf root@${ip}:/etc/nsswitch.conf
+        scp sudo-ldap.conf root@${ip}:/etc/sudo-ldap.conf
     done
 }
 
-ssh_restart(){
+setup_client_ssh(){
     for ip in "${REMOTE_ADDRESS[@]}"
     do
-        echo -e "Setup SSH LDAP Run Remote Server ${ip} restart sshd"
-        ssh root@"${ip}" 'systemctl restart sshd'
-        echo -e "\n"
-    done
-}
-
-setup_client(){
-    for ip in "${REMOTE_ADDRESS[@]}"
-    do
-        echo -e "Setup SSH LDAP Run Remote Server ${ip} Client"
-        ssh root@"${ip}" 'yum install -y openldap-clients nss-pam-ldapd'
-        echo -e "\n"
-    done
-}
-
-client_config(){
-
-    for ip in "${REMOTE_ADDRESS[@]}"
-    do
-        echo -e "Setup SSH LDAP Run Remote Server ${ip} Client"
-        ssh root@"${ip}" "authconfig --enableldap --enableldapauth --enableldaptls --disablesssd --enablemd5 --enablelocauthorize  --enableshadow --ldapserver=ldap-server.magic.com --ldapbasedn='dc=magic,dc=com' --enablemkhomedir --update"
-        ssh root@"${ip}" 'systemctl restart nslcd && systemctl status nslcd'
-        ssh root@"${ip}" 'getent passwd liuyulong'
-        echo -e "\n"
+        ssh root@${ip} "yum -y install openssh-ldap > /dev/null"
+        scp ssh-ldap.conf root@${ip}:/etc/ssh/ldap.conf
+        ssh root@${ip} 'echo -e "AuthorizedKeysCommand /usr/libexec/openssh/ssh-ldap-wrapper\nAuthorizedKeysCommandUser nobody\nPubkeyAuthentication yes" >> /etc/ssh/sshd_config'
+        ssh root@${ip} "systemctl restart sshd && systemctl enable sshd"
     done
     
 }
 
-main(){
-    # for ip in "${REMOTE_ADDRESS[@]}"
-    # do
-    #     echo -e ">>>>>>> $ip\n"
-    #     ping ${ip} -c 1 > /dev/null 2>&1
-    #     if [[ $? -eq 0 ]]; then
-    #         echo -e "ping ${ip} OK\n"
-    #     else
-    #         echo -e "ping ${ip} failed\n"
-    #     fi
-    #     # 如果需要输入密码则会导致超时
-    #     gtimeout 5 ssh root@${ip} echo "SSH has passwordless access"
-    #     if [[ $? -ne 0 ]]; then
-    #         echo "SSH has no passwordless access"
-    #     fi
-    # done
-    setup_client
-    ssh_setup
-    sudo_conf
-    ssh_setup
-    ssh_restart
-    client_config
-}
 
-main
+install_ldap_client
+
+config_rsync
+
+setup_client_ssh
+
+setup_client_authconfig
